84d6a324875c4c10ec2d35c06af1aa50cae07904
Lee Rogers <edwinleerogers@gmail.com>
2020-11-11 17:10:06 -0800

    install router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 // Full class name to use WHEN ITEMS IN CART:
 // "ri-shopping-cart-fill ri-fw ri-2x"
@@ -10,8 +11,12 @@ import React from "react"
 function Header() {
     return (
         <header>
-            <h2>Pic Some</h2>
-            <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+            <Link to="/photos">
+                <h2>Pic Some</h2>
+            </Link>
+            <Link to="/cart">
+                <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+            </Link>
         </header>
     )
 }
